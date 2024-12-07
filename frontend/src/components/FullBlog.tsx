d0165f2585099bae60fb41573f4bf9d5d8953ae1
ikrajnish <kumarajnish20@gmail.com>
2024-12-07 18:41:06 +0530

    type error

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -7,6 +7,7 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
         return <div>Loading...</div>; 
     }
     return <div>
+        
         <Appbar />
         <div className="flex justify-center">
             <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
